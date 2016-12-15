8487e6a94258271b4036dcc62de05a2940d7d436
Hendrikto <hendrik.to@gmail.com>
2016-12-15 17:29:44 +0100

    Add a function to represents cells as characters.

--- snake.c
+++ snake.c
@@ -22,6 +22,19 @@ typedef Cell Board[BOARD_HEIGHT][BOARD_WIDTH];
 
 typedef enum Direction {LEFT, UP, RIGHT, DOWN} Direction;
 
+/**
+ * Represent a cell using a character.
+ *
+ * @return A character representing the content of the cell provided.
+ */
+char representCell(const Cell c) {
+	switch (c) {
+		case SNAKE: return 'S';
+		case FOOD: return 'F';
+		default: return '.';
+	}
+}
+
 int main() {
 	return EXIT_SUCCESS;
 }
